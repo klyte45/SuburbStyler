fdfceae3db7f8df508e4aba3f6d556fcd80fdbe2
boformer <felix.free@gmx.de>
2019-02-24 16:04:23 +0100

    rename all definitions to "Definition", move to separate files

--- Model/DistrictDecorationDefinition.cs
+++ Model/DistrictDecorationDefinition.cs
@@ -2,7 +2,6 @@
 
 namespace Klyte.SuburbStyler.Model
 {
-    #region District Decoration
     public class DistrictDecoration
     {
         public string Name { get; set; }
@@ -42,33 +41,4 @@ namespace Klyte.SuburbStyler.Model
     {
         public int Weight { get; set; }
     }
-    #endregion
-
-    #region Divider Definitions
-    public abstract class DividerDefinition
-    {
-        public string Name { get; set; }
-
-        /// <summary>
-        /// width of the prop (or repeat distance)
-        /// </summary>
-        public float Width { get; set; }
-
-        /// <summary>
-        /// depth of the prop or tree. the generator will inset the dividers by the half depth.
-        /// Can be 0.0 to place props on the lot border.
-        /// </summary>
-        public float Depth { get; set; }
-    }
-
-    public class TreeDividerDefinition : DividerDefinition
-    {
-        public string TreePrefabName { get; set; }
-    }
-
-    public class PropDividerDefinition : DividerDefinition
-    {
-        public string PropPrefabName { get; set; }
-    }
-    #endregion
 }
---
+++ Model/DividerDefinition.cs
@@ -0,0 +1,28 @@
+﻿namespace Klyte.SuburbStyler.Model
+{
+    public abstract class DividerDefinition
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// width of the prop (or repeat distance)
+        /// </summary>
+        public float Width { get; set; }
+
+        /// <summary>
+        /// depth of the prop or tree. the generator will inset the dividers by the half depth.
+        /// Can be 0.0 to place props on the lot border.
+        /// </summary>
+        public float Depth { get; set; }
+    }
+
+    public class TreeDividerDefinition : DividerDefinition
+    {
+        public string TreePrefabName { get; set; }
+    }
+
+    public class PropDividerDefinition : DividerDefinition
+    {
+        public string PropPrefabName { get; set; }
+    }
+}
