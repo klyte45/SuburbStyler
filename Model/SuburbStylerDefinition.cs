a478f0bcd22303b0af38305965192cc89fe4928a
boformer <felix.free@gmx.de>
2019-02-24 16:04:29 +0100

    add top-level definition

---
+++ Model/SuburbStylerDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Klyte.SuburbStyler.Model
+{
+    public class SuburbStylerDefinition
+    {
+        public List<DistrictDecorationDefinition> DistrictDecorationDefinitions { get; set; }
+
+        public List<DividerDefinition> DividerDefinitions { get; set; }
+    }
+}
